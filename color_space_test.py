d0c5089dd9cb96ee441d5c8f8923c153c6f1d92b
bladetragic <bladetragic@gmail.com>
2024-02-12 06:08:04 -0600

    completed rgb to hsv function

--- color_space_test.py
+++ color_space_test.py
@@ -1,14 +1,11 @@
 import numpy as np 
-import matplotlib as plt
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 from skimage import transform
 import skimage.io as io
 import skimage.color as color
 
 def parse_img_array(img):
-    #print("Test\n")
-    #print (img.shape)
-    #print(img)
-    print("\nTest\n")
     for arr in img:
         for pixels in arr:
             print(pixels)
@@ -19,31 +16,32 @@ def parse_img_array(img):
 #Convert RGB values to HSV values
 def rgb_to_hsv(img):
 
+    #Normalize image array
+    img_normalized = img/255.0
+
+    #Create empty array
+    arr2 = np.empty_like(img_normalized)
+
     #Parse Numpy array for r,g,b values
-    for arr in img:
-        for pixels in arr:
-            #print(pixels)
-            r,g,b = pixels[0]/255.0, pixels[1]/255.0, pixels[2]/255.0 #Store rgb channel values
-            # pixels[0] = pixels[0]/255.0
-            # pixels[1] = pixels[1]/255.0 
-            # pixels[2] = pixels[2]/255.0
-            # pixels[0] = r
-            # pixels[1] = g 
-            # pixels[2] = b
-            # print(pixels[0], pixels[1], pixels[2])
+    for row in range(img.shape[0]): 
+        for col in range(img.shape[1]):
+            pixel = img_normalized[row, col]
+            r,g,b = pixel[0], pixel[1], pixel[2] #Store rgb channel values
+            print(f"R, G, B Values: {r} - {g} - {b}")
 
             #Calculate V (value) for HSV color model
-            #v = max(pixels[0], pixels[1], pixels[2])
             v = max(r,g,b)
-            print("V: {}".format(v))
+            print(f"V: {v}")
+
+            #Calculate C (chroma) value
+            c = v - min(r,g,b)
 
             #Calculate S (saturation) for HSV color model; if V=0, then set S = 0
             if v == 0:
                 s = 0
             else:
-                c = v - min(r,g,b)
                 s = c/v
-            print ("S: {}".format(s))
+            print (f"S: {s}")
 
             #Calculate H (hue) for HSV color model
             if c == 0:
@@ -54,78 +52,44 @@ def rgb_to_hsv(img):
                 h = (((b-r)/c) + 2)
             elif v == b:
                 h = (((r-g)/c) + 4)
-            print ("H: {}".format(h))
+            print (f"H: {h}")
 
-            pixels[0], pixels[1], pixels[2] = h, s, v
-    
-    return(img)
+            #Input HSV values into empty array
+            arr2[row, col] = [h, s, v]
 
-    print(img)
-    #r,g,b = r/255.0, g/255.0, b/255.0 #Divide RGB values by 255 to normalize to values between 0 and 1
-    # rgb_max = max(r,g,b)
-    # rgb_min = min(r,g,b)
-    # rgb_diff = rgb_max-rgb_min
+    # Scale H, S, V values to the range [0, 255]
+    arr2[:, :, 0] *= 360  # Scale H to [0, 360]
+    arr2[:, :, 1:] *= 255  # Scale S and V to [0, 255]
 
-    #Calculate V (value) for HSV color model
-    # v = np.max(img, axis = 2) 
-    # #print("V:")
-    # print ("V: {}".format(v))   
+    # Round and convert to uint8
+    arr2 = np.round(arr2).astype(np.uint8)
 
-    #Calculate S (saturation) for HSV color model; if V=0, then set S = 0
-    # if v == 0:
-    #     s = 0
-    # else:
-    #     c = v - np.min(img, axis=2)
-    #     s = c/v
-    # print ("S: {}".format(s))
-    # if c == 0:
-    #     h = 0
-    # elif v == r:
-    #     pass
-            #i = 0
-            # for colors in pixels:
-            #     print(pixels[i])
-            #     i =  i + 1
-        
-        #print(arr)
-        
+    return(arr2)   
 
+def hsv_to_rgb(img):
+    pass
 
 #         0            1           2            3       
 # 0 [ [[255, 0 , 0],[255, 0, 0],[255, 0 , 0],[255, 0 , 0]],
 # 1   [[255, 0 , 0],[255, 0, 0],[255, 0 , 0],[255, 0 , 0]] ]        
 
-# step-2 Define Image size - height, width and depth
-# (rows, columns, dimensions) format
-height, width, channel = 2, 3, 3
+img = plt.imread(r"C:\Users\blade\Pictures\Image_Datasets\misc\4.1.06.tiff")
 
-# step-3 Define Red,Green,Blue Color -for each- 0 to 255
-red, green, blue = 255, 50, 50
+print(img.shape)
 
-# step-4 Generate RGB Numpy Array 
-arr = np.full((height, width, channel), [red, green, blue], dtype=('uint8'))
+plt.imshow(img)
+plt.axis('off')
+plt.title('Tree')
+plt.show()
 
-# step-5 Show inline Image Plot
-#plt.imshow(arr)
+arr2 = rgb_to_hsv(img)
 
-# step-6 Remove edge numbers from Image Plot
-#plt.axis('off')
-
-print(arr)
-
-arr2 = rgb_to_hsv(arr)
-
+#print("ARRAY 1")
+#print(img)
 print(arr2)
+plt.imshow(arr2)
+plt.axis('off')
+plt.title('Tree')
+plt.show()
 
 
-
-
-    
-    # if rgb_diff == 0:
-    #     h = 0
-    # elif rgb_max == r:
-    #     h = ( 60 * (0 + (g-b)/rgb_diff) % 360) #if max=R
-    # elif rgb_max == g:
-    #     h = ( 60 * (2 + (b-r)/(rgb_diff)) % 360) #if max=G
-    # elif rgb_max == b:
-    #     h = ( 60 * (4 + (r-g)/(rgb_diff)) % 360) #if max=B
